7065f310ace7388fe339cde62368be3afd9b7f67
rar1n <bolbat.ilya13@gmail.com>
2025-06-12 11:20:12 +0300

    Приклад
    Приклад

---
+++ CCirclepriklad/CCirclepriklad/fMain.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace CCirclepriklad
+{
+    partial class fMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            pnMain = new Panel();
+            pnTools = new Panel();
+            btnUpFar = new Button();
+            btnUp = new Button();
+            btnCollapse = new Button();
+            btnExpand = new Button();
+            btnDown = new Button();
+            btnDownFar = new Button();
+            btnRight = new Button();
+            btnLeft = new Button();
+            btnRightFar = new Button();
+            btnLeftFar = new Button();
+            btnShow = new Button();
+            btnHide = new Button();
+            btnCreateNew = new Button();
+            cbCircles = new ComboBox();
+            label1 = new Label();
+            pnTools.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnMain
+            // 
+            pnMain.BackColor = Color.White;
+            pnMain.BorderStyle = BorderStyle.FixedSingle;
+            pnMain.Location = new Point(0, -1);
+            pnMain.Name = "pnMain";
+            pnMain.Size = new Size(658, 509);
+            pnMain.TabIndex = 0;
+            // 
+            // pnTools
+            // 
+            pnTools.Controls.Add(btnUpFar);
+            pnTools.Controls.Add(btnUp);
+            pnTools.Controls.Add(btnCollapse);
+            pnTools.Controls.Add(btnExpand);
+            pnTools.Controls.Add(btnDown);
+            pnTools.Controls.Add(btnDownFar);
+            pnTools.Controls.Add(btnRight);
+            pnTools.Controls.Add(btnLeft);
+            pnTools.Controls.Add(btnRightFar);
+            pnTools.Controls.Add(btnLeftFar);
+            pnTools.Controls.Add(btnShow);
+            pnTools.Controls.Add(btnHide);
+            pnTools.Controls.Add(btnCreateNew);
+            pnTools.Controls.Add(cbCircles);
+            pnTools.Controls.Add(label1);
+            pnTools.Location = new Point(657, -1);
+            pnTools.Name = "pnTools";
+            pnTools.Size = new Size(339, 509);
+            pnTools.TabIndex = 1;
+            // 
+            // btnUpFar
+            // 
+            btnUpFar.Image = (Image)resources.GetObject("btnUpFar.Image");
+            btnUpFar.Location = new Point(131, 251);
+            btnUpFar.Name = "btnUpFar";
+            btnUpFar.Size = new Size(75, 30);
+            btnUpFar.TabIndex = 14;
+            btnUpFar.UseVisualStyleBackColor = true;
+            btnUpFar.Click += btnUpFar_Click;
+            // 
+            // btnUp
+            // 
+            btnUp.Image = (Image)resources.GetObject("btnUp.Image");
+            btnUp.Location = new Point(131, 287);
+            btnUp.Name = "btnUp";
+            btnUp.Size = new Size(75, 35);
+            btnUp.TabIndex = 13;
+            btnUp.UseVisualStyleBackColor = true;
+            btnUp.Click += btnUp_Click;
+            // 
+            // btnCollapse
+            // 
+            btnCollapse.Image = (Image)resources.GetObject("btnCollapse.Image");
+            btnCollapse.Location = new Point(131, 364);
+            btnCollapse.Name = "btnCollapse";
+            btnCollapse.Size = new Size(75, 30);
+            btnCollapse.TabIndex = 12;
+            btnCollapse.UseVisualStyleBackColor = true;
+            btnCollapse.Click += btnCollapse_Click;
+            // 
+            // btnExpand
+            // 
+            btnExpand.Image = (Image)resources.GetObject("btnExpand.Image");
+            btnExpand.Location = new Point(131, 328);
+            btnExpand.Name = "btnExpand";
+            btnExpand.Size = new Size(75, 30);
+            btnExpand.TabIndex = 11;
+            btnExpand.UseVisualStyleBackColor = true;
+            btnExpand.Click += btnExpand_Click;
+            // 
+            // btnDown
+            // 
+            btnDown.Image = (Image)resources.GetObject("btnDown.Image");
+            btnDown.Location = new Point(131, 400);
+            btnDown.Name = "btnDown";
+            btnDown.Size = new Size(75, 35);
+            btnDown.TabIndex = 10;
+            btnDown.UseVisualStyleBackColor = true;
+            btnDown.Click += btnDown_Click;
+            // 
+            // btnDownFar
+            // 
+            btnDownFar.Image = (Image)resources.GetObject("btnDownFar.Image");
+            btnDownFar.Location = new Point(131, 441);
+            btnDownFar.Name = "btnDownFar";
+            btnDownFar.Size = new Size(75, 30);
+            btnDownFar.TabIndex = 9;
+            btnDownFar.UseVisualStyleBackColor = true;
+            btnDownFar.Click += btnDownFar_Click;
+            // 
+            // btnRight
+            // 
+            btnRight.Image = (Image)resources.GetObject("btnRight.Image");
+            btnRight.Location = new Point(218, 328);
+            btnRight.Name = "btnRight";
+            btnRight.Size = new Size(30, 66);
+            btnRight.TabIndex = 8;
+            btnRight.UseVisualStyleBackColor = true;
+            btnRight.Click += btnRight_Click;
+            // 
+            // btnLeft
+            // 
+            btnLeft.Image = (Image)resources.GetObject("btnLeft.Image");
+            btnLeft.Location = new Point(89, 328);
+            btnLeft.Name = "btnLeft";
+            btnLeft.Size = new Size(30, 66);
+            btnLeft.TabIndex = 7;
+            btnLeft.UseVisualStyleBackColor = true;
+            btnLeft.Click += btnLeft_Click;
+            // 
+            // btnRightFar
+            // 
+            btnRightFar.Image = (Image)resources.GetObject("btnRightFar.Image");
+            btnRightFar.Location = new Point(254, 328);
+            btnRightFar.Name = "btnRightFar";
+            btnRightFar.Size = new Size(26, 66);
+            btnRightFar.TabIndex = 6;
+            btnRightFar.UseVisualStyleBackColor = true;
+            btnRightFar.Click += btnRightFar_Click;
+            // 
+            // btnLeftFar
+            // 
+            btnLeftFar.Image = (Image)resources.GetObject("btnLeftFar.Image");
+            btnLeftFar.Location = new Point(57, 328);
+            btnLeftFar.Name = "btnLeftFar";
+            btnLeftFar.Size = new Size(26, 66);
+            btnLeftFar.TabIndex = 5;
+            btnLeftFar.UseVisualStyleBackColor = true;
+            btnLeftFar.Click += btnLeftFar_Click;
+            // 
+            // btnShow
+            // 
+            btnShow.Location = new Point(50, 169);
+            btnShow.Name = "btnShow";
+            btnShow.Size = new Size(237, 23);
+            btnShow.TabIndex = 4;
+            btnShow.Text = "Показати об'єкт";
+            btnShow.UseVisualStyleBackColor = true;
+            btnShow.Click += btnShow_Click;
+            // 
+            // btnHide
+            // 
+            btnHide.Location = new Point(50, 140);
+            btnHide.Name = "btnHide";
+            btnHide.Size = new Size(237, 23);
+            btnHide.TabIndex = 3;
+            btnHide.Text = "Приховати об'єкт";
+            btnHide.UseVisualStyleBackColor = true;
+            btnHide.Click += btnHide_Click;
+            // 
+            // btnCreateNew
+            // 
+            btnCreateNew.Location = new Point(50, 111);
+            btnCreateNew.Name = "btnCreateNew";
+            btnCreateNew.Size = new Size(237, 23);
+            btnCreateNew.TabIndex = 2;
+            btnCreateNew.Text = "Створити новий об'єкт";
+            btnCreateNew.UseVisualStyleBackColor = true;
+            btnCreateNew.Click += btnCreateNew_Click;
+            // 
+            // cbCircles
+            // 
+            cbCircles.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbCircles.FormattingEnabled = true;
+            cbCircles.Location = new Point(50, 52);
+            cbCircles.Name = "cbCircles";
+            cbCircles.Size = new Size(237, 23);
+            cbCircles.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Перелік об'єктів:";
+            // 
+            // fMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(996, 507);
+            Controls.Add(pnTools);
+            Controls.Add(pnMain);
+            Name = "fMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Circle Class Demo";
+            pnTools.ResumeLayout(false);
+            pnTools.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnMain;
+        private Panel pnTools;
+        private ComboBox cbCircles;
+        private Label label1;
+        private Button btnShow;
+        private Button btnHide;
+        private Button btnCreateNew;
+        private Button btnUpFar;
+        private Button btnUp;
+        private Button btnCollapse;
+        private Button btnExpand;
+        private Button btnDown;
+        private Button btnDownFar;
+        private Button btnRight;
+        private Button btnLeft;
+        private Button btnRightFar;
+        private Button btnLeftFar;
+    }
+}
